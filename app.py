b87790806578d436ed706954648c3ab1c7b4fa02
oykamal <autocratickamal1@gmail.com>
2021-10-25 22:49:36 +0500

    html done

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello world"
+    # return "hello world"
+    return render_template("index.html")
+
+@app.route("/chat")
+def chat():
+    username = request.args.get("username")
+    room = request.args.get("room")
+    
+    if username and room:
+        return render_template("chat.html", username = username, room = room)
+    else:
+        return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
